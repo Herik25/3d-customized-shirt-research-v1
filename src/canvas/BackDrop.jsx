727e1a9614e7e52bfcf581fbaaca2852bfd5509f
Herik25 <harshparmar87990@gmail.com>
2026-02-28 15:25:49 +0530

    Implement canvas and customizer components with camera rig and shirt rendering; add new components: BackDrop, AiPicker, ColorPicker, FilePicker, and Tab.

---
+++ src/canvas/BackDrop.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BackDrop() {
+  return <div>BackDrop</div>;
+}
+
+export default BackDrop;
--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,7 +1,24 @@
+import { Center, Environment } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 import React from "react";
+import CameraRig from "./CameraRig";
+import BackDrop from "./BackDrop";
+import Shirt from "./Shirt";
 
-function Canvas() {
-  return <div>Canvas</div>;
+function CanvasModel() {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+      <CameraRig>
+      {/* <BackDrop> */}
+      <Center>
+        <Shirt />
+      </Center>
+      {/* </BackDrop> */}
+      </CameraRig>
+    </Canvas>
+  );
 }
 
-export default Canvas;
+export default CanvasModel;
